3cd764333be5c062832adbd1e120a09cda29c708
Phantom <ncwick@student.rtc.edu>
2018-11-29 19:18:10 -0800

    Updated to proper Structure
    Fixed initial setup

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.sql.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+    Connection connect = null;
+
+
+        Connection conn = '(user="root", password="", host="127.0.0.1", database="Computer_Parts")';
+
+    }
+}
+
